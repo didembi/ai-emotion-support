1507f4416458c9edf0a0ee58da250460721b022a
Didem <128546815+didembi@users.noreply.github.com>
2025-08-20 21:02:00 +0300

    langchain-chroma  bağımlılığını ekledim

--- rag/rag_service.py
+++ rag/rag_service.py
@@ -8,7 +8,8 @@ from langchain_community.document_loaders import (
 )
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.vectorstores import Chroma
+# rag_service.py
+from langchain_chroma import Chroma
 from dotenv import load_dotenv  # load_dotenv'i burada da import edelim
 import logging
 
